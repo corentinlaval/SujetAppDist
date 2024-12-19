be21f8ee44ebb36906a8718bd54d698a7773686e
corentinlaval <144536099+corentinlaval@users.noreply.github.com>
2024-12-19 20:54:19 +0100

    add header

--- Front/exam/src/app/app.component.ts
+++ Front/exam/src/app/app.component.ts
@@ -4,10 +4,11 @@ import {AddplayerComponent} from './addplayer/addplayer.component';
 import {SidebarComponent} from './sidebar/sidebar.component';
 import {AddteamComponent} from "./addteam/addteam.component";
 import {TeamRecapComponent} from "./team-recap/team-recap.component";
+import {HeaderComponent} from "./header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [AddteamComponent, SidebarComponent, AddplayerComponent, TeamRecapComponent],
+  imports: [AddteamComponent, SidebarComponent, AddplayerComponent, TeamRecapComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
