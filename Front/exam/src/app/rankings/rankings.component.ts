51532f0784c02a8f0693daa071fc62c6d5b14f09
corentinlaval <144536099+corentinlaval@users.noreply.github.com>
2025-01-07 15:00:14 +0100

    end

--- Front/exam/src/app/rankings/rankings.component.ts
+++ Front/exam/src/app/rankings/rankings.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatchesService } from '../matches.service';
+import {DatePipe, NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-rankings',
-  imports: [],
   templateUrl: './rankings.component.html',
-  styleUrl: './rankings.component.css'
+  styleUrls: ['./rankings.component.css'],
+  imports: [
+    NgForOf,
+    DatePipe,
+    NgIf
+  ]
 })
-export class RankingsComponent {
+export class RankingsComponent implements OnInit {
+  topPlayers: any[] = [];
+  topTeams: any[] = [];
+  upcomingMatches: any[] = [];
 
+  constructor(private matchesService: MatchesService) {}
+
+  ngOnInit(): void {
+    this.loadTopPlayers();
+    this.loadTopTeams();
+    this.loadUpcomingMatches();
+  }
+
+  loadTopPlayers(): void {
+    this.matchesService.getTopPlayers().subscribe((data) => {
+      this.topPlayers = data;
+    });
+  }
+
+  loadTopTeams(): void {
+    this.matchesService.getTopTeams().subscribe((data) => {
+      this.topTeams = data;
+    });
+  }
+
+  loadUpcomingMatches(): void {
+    this.matchesService.getUpcomingMatches().subscribe((data) => {
+      this.upcomingMatches = data;
+    });
+  }
 }
